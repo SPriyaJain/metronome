2dcd55130b177f0d7cb0d40dd1032bfeb2a76270
Samta Priya Jain <priyaj127@hotmail.com>
2016-11-13 18:30:31 -0500

    Change UI and add leading bars

--- metronome.js
+++ metronome.js
@@ -1,30 +1,30 @@
 $(document).ready(function() {
 	var tick = new Audio('tick.mp3');
-	var start = new Audio('start.mp3');
+	var start = new Audio('start.m4a');
 	var count = 0;
 
 	$("#ts").keyup(function(event) {
-	    //if(event.keyCode == 13){
-	        timesig = parseInt($(this).val());
-	    //}
+	    timesig = parseInt($(this).val());
+	});
+
+	$("#bars").keyup(function(event) {
+		bars = parseInt($(this).val());
 	});
 
 	$("#bpm").keyup(function(event) {
-	    //if(event.keyCode == 13){
-	        bpm = 60000 / parseInt($(this).val());
-	        console.log(bpm);
-	    //}
+	    bpm = 60000 / parseInt($(this).val());
 	});
 
 	$("#start").click(function() {
 		window.setInterval(function() {
-			if (count % timesig === 0) {
+			if (count <= bars * timesig) {
+				start.play();
+			} else if (count % timesig === 0) {
 				start.play()
 			} else {
 				tick.play()
 			}
 			count++;
-			console.log(timesig);
-		}, bpm)
+		}, bpm);
 	});
 });
