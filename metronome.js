3819a767c839251286bbd53d38ac1a95ee75f07f
Samta Priya Jain <priyaj127@hotmail.com>
2017-08-07 00:34:18 -0400

    UI changes

--- metronome.js
+++ metronome.js
@@ -2,16 +2,17 @@ $(document).ready(function() {
 	var tick = new Audio('tick.mp3');
 	var start = new Audio('start.m4a');
 	var count = 0;
+	var formCurTop = "135";
 
-	$("#ts").keyup(function(event) {
+	$(".ts").keyup(function(event) {
 	    timesig = parseInt($(this).val());
 	});
 
-	$("#bars").keyup(function(event) {
+	$(".bars").keyup(function(event) {
 		bars = parseInt($(this).val());
 	});
 
-	$("#bpm").keyup(function(event) {
+	$(".bpm").keyup(function(event) {
 	    bpm = 60000 / parseInt($(this).val());
 	});
 
@@ -26,5 +27,14 @@ $(document).ready(function() {
 			}
 			count++;
 		}, bpm);
+		$(".ts").prop("disabled", true);
+		$(".bars").prop("disabled", true);
+		$(".bpm").prop("disabled", true);
+		$("#start").css("pointer-events", "none");
+		$("#start").prop("disabled", true);
 	});
+
+	$(".add").click(function() {
+		alert("Coming soon");
+	})
 });
